25a19359767f0cc41ef6c9241ba2d48927055853
Skylar Hu <skylarhyn@gmail.com>
2025-06-08 22:12:08 +0100

    LC-valid-perfect-square [Runtime: 0 ms; Memory: 12.6 MB]

---
+++ math/valid-perfect-square.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num < 2:
+            return True
+        
+        left, right = 2, num // 2
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
