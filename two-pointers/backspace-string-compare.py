fbd89d282b512206954437abeb07fc58d8ffa0be
Skylar Hu <skylarhyn@gmail.com>
2025-06-09 00:47:45 +0100

    LC-backspace-string-compare [Runtime: 1 ms; Memory: 12.3 MB]

--- two-pointers/backspace-string-compare.py
+++ two-pointers/backspace-string-compare.py
@@ -5,11 +5,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        """
+        Time: O(n + m), where n and m are lengths of s and t
+        Space: O(n + m) for the stacks
+        """
         def process(string):
             stack = []
             for char in string:
                 if char == '#':
-                    if stack:
+                    if stack:  # Check if the stack is empty or not
                         stack.pop()
                 else:
                     stack.append(char)
